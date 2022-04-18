85ec1b2e74e5d4df8d49e1b310f373f1a6404df4
mohdnuman <mohammadnuman123@gmail.com>
2022-04-18 23:30:49 +0530

    compressed

--- smartContract.js
+++ smartContract.js
@@ -1,9 +1,7 @@
 const Web3 = require("web3");
 
 const stakingAbi = require("./abi.json");
-const tokenAbi=require("./abi2.json")
-;
-const readline = require("readline-sync");
+const tokenAbi = require("./abi2.json");
 
 let web3;
 
@@ -12,30 +10,21 @@ const provider = new Web3.providers.HttpProvider(
 );
 web3 = new Web3(provider);
 
-const alphaStaking="0x2aA297c3208bD98a9a477514d3C80ace570A6deE";
-const token="0xa1faa113cbE53436Df28FF0aEe54275c13B40975"
+async function getBalance(address) {
+  const alphaStaking = "0x2aA297c3208bD98a9a477514d3C80ace570A6deE";
+  const token = "0xa1faa113cbE53436Df28FF0aEe54275c13B40975";
 
-const stakingInstance = new web3.eth.Contract(
-  stakingAbi,
-  alphaStaking
-);
-const tokenInstance = new web3.eth.Contract(
-  tokenAbi,
-  token
-);
+  const stakingInstance = new web3.eth.Contract(stakingAbi, alphaStaking);
+  const tokenInstance = new web3.eth.Contract(tokenAbi, token);
 
-async function getBalance() {
-  let address = readline.question("enter address:");
-  console.log("ALPHA staking:");
+  const decimals = await tokenInstance.methods.decimals().call();
+  const symbol = await tokenInstance.methods.symbol().call();
+  const stake = await stakingInstance.methods.getStakeValue(address).call();
 
-  const decimals=await tokenInstance.methods.decimals().call();
-  const stake= await stakingInstance.methods.getStakeValue(address).call();
+  const balance = (stake / 10 ** decimals).toFixed(2);
 
-  const balance=(stake/(10**decimals)).toFixed(2)
-
-  console.log('balance:',balance,'ALPHA');
-
+  console.log("balance:", balance, symbol);
 }
 
-getBalance();
-
+let address = "0xbd9ac6dd60e769d4300b16b573846789306ae56c";
+getBalance(address);
